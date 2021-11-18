f30e88a5d64225ac0d5e1fd5ef2668f7bd4dc024
Damian Oberholzer <damobe@az.zli.ch>
2021-11-18 13:56:00 +0100

    Note (Model) vorbereitet

--- app/src/main/java/ch/zli/m335/androidnotes/Model/Note.java
+++ app/src/main/java/ch/zli/m335/androidnotes/Model/Note.java
@@ -1,2 +1,29 @@
-package ch.zli.m335.androidnotes.Model;public class Note {
+package ch.zli.m335.androidnotes.Model;
+
+public class Note {
+
+    private String title;
+    private String text;
+
+    public Note(String title, String text) {
+        super();
+        this.title = title;
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }

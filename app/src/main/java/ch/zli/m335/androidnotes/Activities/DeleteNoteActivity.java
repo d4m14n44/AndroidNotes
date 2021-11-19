9395a03861d15ba66623ab73426f2c5a6e5b4082
Damian Oberholzer <damobe@az.zli.ch>
2021-11-19 08:22:45 +0100

    Delete View und Activity implementiert

--- app/src/main/java/ch/zli/m335/androidnotes/Activities/DeleteNoteActivity.java
+++ app/src/main/java/ch/zli/m335/androidnotes/Activities/DeleteNoteActivity.java
@@ -1,6 +1,50 @@
 package ch.zli.m335.androidnotes.Activities;
 
+import android.app.AlarmManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import ch.zli.m335.androidnotes.MainActivity;
+import ch.zli.m335.androidnotes.R;
+
 public class DeleteNoteActivity extends AppCompatActivity {
+
+    private Button yes;
+    private Button no;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_note_activity);
+
+        this.yes = (Button) findViewById(R.id.yesButton);
+        this.no = (Button) findViewById(R.id.noButton);
+
+
+    }
+
+    private OnClickListener deleteNote = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(DeleteNoteActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    private OnClickListener cancelDelete = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(DeleteNoteActivity.this, EditNoteActivity.class);
+            startActivity(intent);
+        }
+    };
+
+
 }
